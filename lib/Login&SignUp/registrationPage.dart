061d2be849078795a6ff9852a70e1e7de605b32f
jamesozs7890 <61954268+jamesozs7890@users.noreply.github.com>
2020-10-18 21:50:14 +0800

    Color changed

--- lib/Login&SignUp/registrationPage.dart
+++ lib/Login&SignUp/registrationPage.dart
@@ -1,6 +1,8 @@
 //import 'package:tvf_legion/loginPage.dart';
 import 'package:flutter/material.dart';
+import 'package:tvf_legion/Login&SignUp/loginPage.dart';
 import 'file:///C:/Users/James/Desktop/DetailBooking/Booking-Detail/tvf_legion/lib/Login&SignUp/registrationNextPage.dart';
+import 'package:tvf_legion/services/auth.dart';
 
 class Registration extends StatefulWidget {
   @override
@@ -22,6 +24,9 @@ class _RegistrationState extends State<Registration> {
   @override
   Widget build(BuildContext context) {
     final firstNameTextField = TextFormField(
+      validator: (val){
+        return val.isEmpty && val.length<3? "Invalid Entry": null;
+      },
         obscureText: false,
         controller: firstNameController,
         style: style,
@@ -34,6 +39,9 @@ class _RegistrationState extends State<Registration> {
     );
 
     final lastNameTextField = TextFormField(
+        validator: (val){
+          return val.isEmpty && val.length<3? "Invalid Entry": null;
+        },
         obscureText: false,
         controller: lastNameController,
         style: style,
@@ -46,6 +54,9 @@ class _RegistrationState extends State<Registration> {
     );
 
     final emailTextField = TextFormField(
+        validator: (val){
+          return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? null : "Enter a proper email";
+        },
         obscureText: false,
         controller: emailController,
         style: style,
@@ -58,6 +69,9 @@ class _RegistrationState extends State<Registration> {
     );
 
     final passwordTextField = TextFormField(
+        validator:  (val){
+          return val.length < 6 ? "Please re-enter password" : null;
+        },
         obscureText: true,
         controller: passwordController,
         style: style,
@@ -70,6 +84,9 @@ class _RegistrationState extends State<Registration> {
     );
 
     final confirmPasswordTextField = TextFormField(
+        validator:  (val){
+          return val.length < 6? "Please re-enter password" : null;
+        },
         obscureText: true,
         controller: cPasswordController,
         style: style,
@@ -81,6 +98,33 @@ class _RegistrationState extends State<Registration> {
         )
     );
 
+    bool isLoading = false;
+    AuthMethods authMethods= new AuthMethods();
+
+    signUp()async{
+      if(fKey.currentState.validate()){
+        setState(() {
+          isLoading = true;
+        });
+
+        await authMethods.signUp(emailController.text, passwordController.text).then(
+            (result){
+              if(result!=null){
+                Navigator.pushReplacement(context, MaterialPageRoute(
+                  builder: (context) => LoginPage()
+                ));
+              }
+            });
+      }
+    }
+
+    newSignUp(){
+      if(fKey.currentState.validate()){
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Registration2()));
+      }
+    }
+
     final nextButton = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
@@ -89,8 +133,7 @@ class _RegistrationState extends State<Registration> {
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => Registration2()));
+          newSignUp();
         },
         child: Text("Next",
             textAlign: TextAlign.right,
@@ -101,46 +144,53 @@ class _RegistrationState extends State<Registration> {
     );
 
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 100.0,
-                    child: Image.asset(
-                      "assets/images/logo.png",
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SizedBox(width: 5.0, height: 30.0),
-                      new Flexible(child: firstNameTextField),
-                      SizedBox(width: 5.0, height: 30.0),
-                      new Flexible(child: lastNameTextField),
-                    ],
-                  ),
-                  SizedBox(height: 15.0),
-                  emailTextField,
-                  SizedBox(height: 15.0),
-                  passwordTextField,
-                  SizedBox(height: 15.0),
-                  confirmPasswordTextField,
-                  SizedBox(height: 25.0),
-                  nextButton,
-                ],
-              ),
-            ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xff01A0C7),
+        title: Text(
+          "Sign Up",
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold
           ),
         ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 100.0,
+            child: Image.asset(
+              "assets/images/logo.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+
+          Form(
+            key:fKey,
+            child: Column(
+              children: <Widget>[
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(width: 5.0, height: 30.0),
+                    new Flexible(child: firstNameTextField),
+                    SizedBox(width: 5.0, height: 30.0),
+                    new Flexible(child: lastNameTextField),
+                  ],
+                ),
+                SizedBox(height: 15.0),
+                emailTextField,
+                SizedBox(height: 15.0),
+                passwordTextField,
+                SizedBox(height: 15.0),
+                confirmPasswordTextField,
+              ],
+            ),
+          ),
+
+          SizedBox(height: 25.0),
+          nextButton,
+        ],
       ),
     );
   }
