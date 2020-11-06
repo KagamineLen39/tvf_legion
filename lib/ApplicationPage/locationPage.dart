4229d139f7585450b1c5d0ba13ffc224323f00eb
jamesozs7890 <61954268+jamesozs7890@users.noreply.github.com>
2020-11-06 22:58:46 +0800

    Live Location Locator Implemented

--- lib/ApplicationPage/locationPage.dart
+++ lib/ApplicationPage/locationPage.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geoflutterfire/geoflutterfire.dart';
 
 class LocationPage extends StatefulWidget {
 
@@ -12,7 +12,6 @@ class LocationPage extends StatefulWidget {
 
 class _LocationPageState extends State<LocationPage> with TickerProviderStateMixin {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-
   @override
   Widget build(BuildContext context) {
 
@@ -30,14 +29,36 @@ class FireMap extends StatefulWidget {
 class _FireMapState extends State<FireMap> {
   Completer<GoogleMapController> _googleMapController = Completer();
   GoogleMapController newMapController;
+
+  Position currentPosition;
+
+  var geoLocator = Geolocator();
+
+  CameraPosition cameraPosition;
+
+  void locateUser()async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    currentPosition= position;
+
+    LatLng latLngPos = LatLng(position.latitude, position.longitude);
+
+    cameraPosition = new CameraPosition(target: latLngPos,zoom:15);
+
+    newMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
   build(context){
     return Stack(children: [
 
       GoogleMap(
+        initialCameraPosition: CameraPosition(target: LatLng(5.4164, 100.3327),zoom: 15),
         onMapCreated: _onMapCreated,
         myLocationButtonEnabled: true,
-        mapType: MapType.hybrid,
+        myLocationEnabled: true,
+        mapType: MapType.normal,
         compassEnabled: true,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
       ),
 
     ],);
@@ -46,6 +67,8 @@ class _FireMapState extends State<FireMap> {
   _onMapCreated(GoogleMapController control){
     _googleMapController.complete(control);
     newMapController = control;
+
+    locateUser();
   }
 
 }
