d33d59437611cbffd4723071d7624fb20a448cea
jamesozs7890 <61954268+jamesozs7890@users.noreply.github.com>
2020-11-09 00:33:17 +0800

    Merge Success

--- lib/ApplicationPage/homePage.dart
+++ lib/ApplicationPage/homePage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:tvf_legion/ApplicationPage/locationPage.dart';
-import 'package:tvf_legion/ApplicationPage/profilePage.dart';
+import 'package:tvf_legion/ApplicationPage/SearchPage.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -11,71 +9,117 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-  int _page = 0;
-
-  PageController _pageController = PageController(
-    initialPage: 0,
-  );
-
-  List<Widget> _tabBar = [
-    Container(
-      color: Colors.white,
-    ),
-    Container(
-      color: Colors.blue,
-    ),
-    LocationPage(),
-
-    ProfilePage(),
-  ];
 
   @override
   Widget build(BuildContext context) {
-    final homeNavigationBar = CurvedNavigationBar(
-        index: _page,
-        items: [
-          new Icon(Icons.home, size: 30),
-          new Icon(Icons.chat, size: 30),
-          new Icon(Icons.location_on, size: 30),
-          new Icon(Icons.person, size: 30)
-        ],
-        color: Colors.white,
-        buttonBackgroundColor: Colors. red,
-        backgroundColor: Color(0xff01A0C7),
-        onTap: (index){
-          _page = index;
-          _pageController.animateToPage(index, duration:Duration(milliseconds: 500), curve: Curves.easeIn, );
-          setState(() {});
-        }
-    );
 
+    final welcomeLabel = Text("Welcome, \nUsername",
+        style: TextStyle(
+          fontFamily: 'Montserrat',
+          fontSize: 40.0,
+          fontWeight: FontWeight.bold,
+        ));
 
-    return Scaffold(
-      bottomNavigationBar : homeNavigationBar,
-      appBar: AppBar(
-        backgroundColor: Color(0xff01A0C7),
-        centerTitle: true,
-        title: Text(
-          "Home Page",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
+    final createRoomLabel = Text("Create a room",
+        style: TextStyle(
+          fontFamily: 'Montserrat',
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ));
+
+    final createButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text(
+            "Create Group",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold),
         ),
       ),
+    );
 
-      body: PageView(
-        physics: NeverScrollableScrollPhysics(),
-        controller: _pageController,
-        onPageChanged: (index){
-          setState(() {
-            _page = index;
-          });
+    final searchField = GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SearchPage())
+          );
         },
-        children: _tabBar ,
+        child: TextField(
+            enabled: false,
+            readOnly: true,
+            decoration: InputDecoration(
+              labelText: "Search for group to join",
+              hintText: "Search",
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0))),
+            )));
+
+    final roomNameLabel = Text("Rooms",
+        style: TextStyle(
+          fontFamily: 'Montserrat',
+          fontSize: 35.0,
+          fontWeight: FontWeight.bold,
+        ));
+
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+            child: Column(
+
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.red, Colors.blue],
+                        begin: const FractionalOffset(0.0, 0.0),
+                        end: const FractionalOffset(0.5, 0.0),
+                        stops: [0.0, 1.0],
+
+
+                      ),
+                    ),
+                    child: Column(
+
+                        children: <Widget>[
+                          welcomeLabel,
+                        ]),
+                  ),
+                  Container(
+                    height: 300.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/discussion.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter: new ColorFilter.mode(
+                            Colors.black.withOpacity(0.4), BlendMode.dstATop),
+                      ),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        createRoomLabel,
+                        SizedBox(width: 10, height: 200),
+                        createButton,
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  searchField,
+                  SizedBox(height: 20),
+                  roomNameLabel
+                ])),
       ),
     );
   }
-
 }
---
+++ lib/ApplicationPage/navigationBar.dart
@@ -0,0 +1,67 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:tvf_legion/ApplicationPage/homePage.dart';
+import 'package:tvf_legion/ApplicationPage/profilePage.dart';
+
+class NavigationPage extends StatefulWidget {
+  @override
+  _NavigationPageState createState() => _NavigationPageState();
+}
+
+class _NavigationPageState extends State<NavigationPage>
+    with TickerProviderStateMixin {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  int _page = 0;
+
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
+  List<Widget> _tabBar = [
+    HomePage(),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final homeNavigationBar = CurvedNavigationBar(
+        index: _page,
+        items: [
+          new Icon(Icons.home, size: 30),
+          new Icon(Icons.chat, size: 30),
+          new Icon(Icons.location_on, size: 30),
+          new Icon(Icons.person, size: 30)
+        ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.red,
+        backgroundColor: Color(0xff01A0C7),
+        onTap: (index) {
+          _page = index;
+          _pageController.animateToPage(
+            index,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeIn,
+          );
+          setState(() {});
+        });
+
+    return Scaffold(
+      bottomNavigationBar: homeNavigationBar,
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+        children: _tabBar,
+      ),
+    );
+  }
+}
