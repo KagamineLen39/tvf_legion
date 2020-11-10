b2039b87a2ce0fd8fa80bff301266d27b14da8dd
jamesozs7890 <61954268+jamesozs7890@users.noreply.github.com>
2020-11-10 18:00:25 +0800

    Revert "Fix major flaws"
    This reverts commit e8f0bc3af7a3162ca7936b3b21f8f544cdb3ee54.

--- lib/ApplicationPage/homePage.dart
+++ lib/ApplicationPage/homePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:tvf_legion/ApplicationPage/locationPage.dart';
+import 'package:tvf_legion/ApplicationPage/profilePage.dart';
 import 'package:tvf_legion/ApplicationPage/SearchPage.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +13,69 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
+  int _page = 0;
+
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
+  List<Widget> _tabBar = [
+    _homePage(),
+
+    Container(
+      color: Colors.white,
+    ),
+
+    LocationPage(),
+
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    final homeNavigationBar = CurvedNavigationBar(
+        index: _page,
+        items: [
+          new Icon(Icons.home, size: 30,color:Colors.white),
+          new Icon(Icons.chat, size: 30,color: Colors.white),
+          new Icon(Icons.location_on, size: 30,color: Colors.white),
+          new Icon(Icons.person, size: 30,color: Colors.white)
+        ],
+        color: Colors.lightBlue[200],
+        buttonBackgroundColor: Colors.lightBlue[500],
+        backgroundColor: Colors.blue[700],
+        onTap: (index){
+          _page = index;
+          _pageController.animateToPage(index, duration:Duration(milliseconds: 500), curve: Curves.easeIn, );
+          setState(() {});
+        }
+    );
+
+    return Scaffold(
+      bottomNavigationBar : homeNavigationBar,
+
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        onPageChanged:(index){
+          setState(() {
+            _page = index;
+          });
+        },
+        children: _tabBar,
+      ),
+    );
+  }
+
+}
+
+class _homePage extends StatefulWidget {
+  @override
+  __homePageState createState() => __homePageState();
+}
+
+class __homePageState extends State<_homePage> {
   @override
   Widget build(BuildContext context) {
 
